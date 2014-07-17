55c7ee4cecef6be88ee258b45f17e8525a48ac79
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 09:57:32 +0900

    Omit spec descriptions

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,10 @@ describe MessageFilter do
   before do
     @filter = MessageFilter.new('foo')
   end
-  it 'detects message with NG word' do
+  it {
     expect(@filter).to be_detect('hello from foo')
-  end
-  it 'does not detect message without NG word' do
+  }
+  it {
     expect(@filter).not_to be_detect('hello, world')
-  end
+  }
 end
