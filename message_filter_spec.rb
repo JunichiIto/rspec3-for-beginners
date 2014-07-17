98b1825bc9ba758ffa879d1dfefdea56cf781565
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 10:03:07 +0900

    Use subject

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,11 @@ describe MessageFilter, 'with argument "foo"' do
   before do
     @filter = MessageFilter.new('foo')
   end
+  subject { @filter }
   it {
-    expect(@filter).to be_detect('hello from foo')
+    is_expected.to be_detect('hello from foo')
   }
   it {
-    expect(@filter).not_to be_detect('hello, world')
+    is_expected.not_to be_detect('hello, world')
   }
 end
