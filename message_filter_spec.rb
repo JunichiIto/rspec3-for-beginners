c63c8dc12cd00b06b10dfe80f7330c124e568da1
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 09:39:06 +0900

    Add triangulation spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,4 +5,8 @@ describe MessageFilter do
     filter = MessageFilter.new('foo')
     expect(filter.detect?('hello from foo')).to eq true
   end
+  it 'does not detect message without NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello, world!')).to eq false
+  end
 end
