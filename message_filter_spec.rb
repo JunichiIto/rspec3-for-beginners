196a261be180f203f949ee569cc7b4a5d4940756
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 09:32:08 +0900

    Add first spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,4 +1,8 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
+  it 'detects message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to eq true
+  end
 end
