1d8abc0a708b194193dcb575e46cfbd0f95e6ec0
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 09:52:35 +0900

    Use be_predicate matcher

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,9 +5,9 @@ describe MessageFilter do
     @filter = MessageFilter.new('foo')
   end
   it 'detects message with NG word' do
-    expect(@filter.detect?('hello from foo')).to eq true
+    expect(@filter).to be_detect('hello from foo')
   end
   it 'does not detect message without NG word' do
-    expect(@filter.detect?('hello, world!')).to eq false
+    expect(@filter).not_to be_detect('hello, world')
   end
 end
