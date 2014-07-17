674023851dc186c527f8d56c26b350c61910f2d0
Junichi Ito <jit@sonicgarden.jp>
2014-07-17 10:06:29 +0900

    Join lines

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,10 +2,6 @@ require_relative 'message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it {
-    is_expected.to be_detect('hello from foo')
-  }
-  it {
-    is_expected.not_to be_detect('hello, world')
-  }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world') }
 end
