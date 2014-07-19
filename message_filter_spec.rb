111454e3b336690812d82ed377454206c3b2e40d
Junichi Ito <jit@sonicgarden.jp>
2014-07-20 05:08:44 +0900

    Move its-example to shared_examples

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -5,11 +5,11 @@ describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
     it { is_expected.to be_detect('hello from foo') }
     it { is_expected.not_to be_detect('hello, world!') }
+    its(:ng_words) { is_expected.not_to be_empty }
   end
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
-    its(:ng_words) { is_expected.not_to be_empty }
   end
   context 'with argument "foo","bar"' do
     subject { MessageFilter.new('foo', 'bar') }
