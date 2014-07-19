31c64767f2ed0fc866c636df870abb2b9eb89d96
Junichi Ito <jit@sonicgarden.jp>
2014-07-20 03:42:29 +0900

    Use shared_examples

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,14 +1,17 @@
 require_relative 'message_filter'
 
+shared_examples 'MessageFilter with argument "foo"' do
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world!') }
+end
+
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it { is_expected.to be_detect('hello from foo') }
-  it { is_expected.not_to be_detect('hello, world') }
+  it_behaves_like 'MessageFilter with argument "foo"'
 end
 
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo', 'bar') }
   it { is_expected.to be_detect('hello from bar') }
-  it { is_expected.to be_detect('hello from foo') }
-  it { is_expected.not_to be_detect('hello, world!') }
+  it_behaves_like 'MessageFilter with argument "foo"'
 end
