fb0a27e5700aab36e8a19ce3e4ba1118efded56a
Junichi Ito <jit@sonicgarden.jp>
2014-07-20 03:35:07 +0900

    Add specs for variable length argument

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -9,4 +9,6 @@ end
 describe MessageFilter, 'with argument "foo","bar"' do
   subject { MessageFilter.new('foo', 'bar') }
   it { is_expected.to be_detect('hello from bar') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world!') }
 end
